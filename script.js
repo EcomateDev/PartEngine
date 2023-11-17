758c59e8b1e1711bde63a9797bcd7b421c7700f4
EcomateDev <144458220+EcomateDev@users.noreply.github.com>
2023-11-17 15:04:31 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.querySelector('.invite-btn').addEventListener('click', () => {
+  window.open('https://discord.com/api/oauth2/authorize?client_id=1174691560055648276&permissions=8&scope=bot', '_blank');
+});
